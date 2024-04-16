264be4821419a440addd94454fe48150e6350c7c
kkravchenko <klymkravchenko@ecisys.com>
2024-04-16 12:31:11 +0300

    transaction work

--- src/features/account/account.service.ts
+++ src/features/account/account.service.ts
@@ -269,7 +269,7 @@ export class AccountService {
     const data = pipe(
       drop(offset),
       take(limit),
-      orderBy(['createdAt'], ['asc']),
+      orderBy([(it: Transaction) => new Date(it.createdAt)], ['asc']),
     )(transactions) as Transaction[];
 
     return {
